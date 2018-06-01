79e72bf84f7a3d5ff57d4e2199be2fa26f4c695e
Konstantin Volodin <volodin.konstantin@gmail.com>
2018-06-01 14:51:04 +0300

    install opencv to this computer.
    install opencv to this computer.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+apt-get update
+apt-get install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg 
